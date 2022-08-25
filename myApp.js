cb45f68b6e34036a9e765820f93ef925881b7aa6
radicaled <daniel.bianco@outlook.com>
2022-08-24 21:14:14 -0300

    commented send file

--- myApp.js
+++ myApp.js
@@ -10,6 +10,6 @@ absolutePath = __dirname + "/views/index.html";
 console.log(absolutePath);
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  //   res.send("Hello Express");
   res.sendFile(absolutePath);
 });
