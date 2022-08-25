a52f02c545826422f801ec845e035225c08c47ca
radicaled <daniel.bianco@outlook.com>
2022-08-24 21:27:16 -0300

    add simple api request rply json

--- myApp.js
+++ myApp.js
@@ -9,9 +9,16 @@ absolutePath = __dirname + "/views/index.html";
 
 console.log(absolutePath);
 
+// Add static path to use the files under /public
 app.use("/public", express.static(__dirname + "/public"));
 
+// Frontend
 app.get("/", (req, res) => {
   //   res.send("Hello Express");
   res.sendFile(absolutePath);
+});
+
+// Simple API request
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
 });
