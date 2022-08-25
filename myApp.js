d73da73456504215efb4ff83567c854b18f1b982
radicaled <daniel.bianco@outlook.com>
2022-08-24 21:12:05 -0300

    send a file

--- myApp.js
+++ myApp.js
@@ -5,6 +5,11 @@ console.log("Hello Express");
 
 module.exports = app;
 
+absolutePath = __dirname + "/views/index.html";
+
+console.log(absolutePath);
+
 app.get("/", (req, res) => {
   res.send("Hello Express");
+  res.sendFile(absolutePath);
 });
